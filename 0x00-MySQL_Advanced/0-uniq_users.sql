a307c495983590a9291550a03dff16eb89dacee4
Roro1839 <ronaldokipkirui90@gmail.com>
2023-10-12 16:55:24 -0700

    mysql

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,11 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+CREATE TABLE users (
+	-- Unique identifier for each user
+	id INTEGER PRIMARY KEY  AUTO_INCREMENT,
+	-- String (255 characters), never null and unique
+       	email VARCHAR(255) NOT NULL UNIQUE,
+
+	-- Name of the user (maximum 255 characters)
+       	name VARCHAR(255)
+);
