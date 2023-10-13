8ce6363d64b444cc72c71f61857a7ad71cbafd76
Roro1839 <ronaldokipkirui90@gmail.com>
2023-10-12 17:18:42 -0700

    mysql

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,12 @@
+--Create table users which doesn't exit
+CREATE TABLE users (
+-- Id should be int never nulll
+id INTEGER AUTO_INCREMENT PRIMARY KEY,
+-- Email unique maximum (255 char)
+email VARCHAR(255) NOT  NULL UNIQUE,
+
+-- Name should be a maximum (255 char)
+name VARCHAR(255),
+
+country ENUM('US','CO' and 'TN') NOT NULL DEFAULT 'US'
+);
